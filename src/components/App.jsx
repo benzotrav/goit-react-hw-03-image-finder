22306a28e4caccc81744e24ccad2fb19b574c7cd
benzotrav <93350704+benzotrav@users.noreply.github.com>
2022-07-11 20:24:09 +0300

    searchbar done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
-export const App = () => {
+import { Searchbar } from "./Searchbar/Searchbar";
+import { Component } from "react";
+
+
+
+export class App extends Component {
+  render()   {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+  <Searchbar  onSubmit={this.setQuery}/>
+  )
+}
+}
