89bb9db5a0217a345852666a287ba266a404f99d
benzotrav <93350704+benzotrav@users.noreply.github.com>
2022-07-12 12:08:12 +0300

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,100 +9,131 @@ import { Modal } from "./Modal/Modal";
 
 
 
-
 export class App extends Component { 
-    state = {
-      isLoading: false,
-      page: 1,
-      data: [],
-      total: 0,
-      pages: 0,
-      error: '',
-      query: '',
-      showLargePic: false,
-      picData: {},
-    };
-  
-    async componentDidUpdate(prevProps, prevState) { 
-      const { query, page } = this.state; 
-      const { query: prevQuery, page: prevPage } = prevState; 
-  
-      if (query !== prevQuery || (page !== prevPage && page !== 1)) { 
-        API.params.page = query !== prevQuery ? 1 : page;
-        API.params.q = query;
-        try {
-          this.setState({ isLoading: true });
-          const data = await API.getData(API.params);
-          const { total, hits } = data;
-  
-          const properStructHits = hits.map(({ id, largeImageURL, webformatURL, tags }) => ({
-                id,
-                largeImageURL,
-                webformatURL,
-                tags,
-              }))
-  
-          if (query !== prevQuery) {
-            this.setState({
-              data: [...properStructHits],
-              page: API.params.page,
-              total: total,
-              pages: Math.ceil(total / API.params.per_page),
-              isLoading: false,
-            });
-          } else {
-            this.setState(p => ({
-              data: [
-                ...p.data,
-                ...properStructHits,
-              ],
-              page: API.params.page,
-              isLoading: false,
-            }));
-          }
-        } catch (error) {
-          this.setState({ error: true, isLoading: false });
-          console.log(error);
-        }
-      }
+  state = {
+    query: '',
+    data: [],
+    page: 1,
+    error: null,
+    status: 'idle',
+    showModal: false,
+    imgId: null,
+    total: 0,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.query !== prevState.query) {
+      this.setState({ status: 'pending', data: [], page: 1 }, this.getPicture);
     }
-  
-    setQuery = value => {
-      this.setState({ query: value });
-    };
-  
-    toggleLargeMode = picData => {
-      this.setState(({ showLargePic }) => ({
-        showLargePic: !showLargePic,
-        picData,
-      }));
-    };
-  
-    handleLoadMore = () => {
-      this.setState(p => ({ page: p.page + 1 }));
-    };
+    if (this.state.page !== prevState.page && this.state.page !== 1) {
+      this.setState({ status: 'pending' }, this.getPicture);
+    }
+  }
+
+  getPicture = () => {
+    const { query } = this.state;
+    const { page } = this.state;
+    API(query, page)
+      .then(this.dataProcessing)
+      .catch(error => this.setState({ error, status: 'rejected' }));
+  };
+
+  dataProcessing = response => {
+    const { hits: dataArray, totalHits } = response.data;
+
+    if (!dataArray.length) {
+      this.setState({
+        status: 'rejected',
+        error: new Error('Try to change the request'),
+      });
+      return;
+    }
+    window.scrollBy({
+      top: document.body.clientHeight,
+      behavior: 'smooth',
+    });
+
+    const newData = dataArray.map(data => {
+      const {
+        id,
+        largeImageURL: imageURL,
+        webformatURL: src,
+        tags: alt,
+      } = data;
+      return { id, imageURL, src, alt };
+    });
+    return this.setState(({ data }) => {
+      return {
+        data: [...data, ...newData],
+        total: totalHits,
+        status: 'resolved',
+      };
+    });
+  };
+
+  handleSubmit = searchQuery => {
+    if (this.state.query !== searchQuery) {
+      this.setState({ query: searchQuery });
+    }
+    return;
+  };
+
+  handleLoadMore = () => {
+    this.setState(({ page }) => {
+      return { page: page + 1 };
+    });
+  };
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  };
+
+  clickOnImage = id => {
+    this.setState({ imgId: id });
+    this.toggleModal();
+  };
+
+  handleData = () => {
+    return this.state.data.find(img => img.id === this.state.imgId);
+  };
+
   
     render() {
-      const { data, isLoading, page, pages, showLargePic, picData } = this.state;
+      const { status, error, data, showModal, total } = this.state;
+
   
       return (
         <AppBox>
-          <Searchbar onSubmit={this.setQuery} />
-          {data.length > 0 && (
-            <ImageGallery data={data} toggleLargeMode={this.toggleLargeMode} />
-          )}
-          {isLoading && <Loader />}
-          {data.length > 0 && page < pages && (
-            <Button type="button" onClick={this.handleLoadMore}>
-              Load more
-            </Button>
-          )}
-          {showLargePic && (
-            <Modal onClose={this.toggleLargeMode}>
-              <img alt={picData.alt} src={picData.url} />
-            </Modal>
-          )}
-        </AppBox>
+        <Searchbar onSubmit={this.handleSubmit} />
+        {data.length > 0 && (
+          <ImageGallery data={this.state.data} onClick={this.clickOnImage} />
+        )}
+        {status === 'resolved' && data.length > 0 && data.length < total && (
+          <>
+            <Button onClick={this.handleLoadMore} />
+          </>
+        )}
+
+        {status === 'pending' && (          
+            <Loader
+              color="#00BFFF"
+              height={200}
+              width={200}
+              ariaLabel="loading"
+            />
+        )}
+        {status === 'rejected' && (
+          <div className={ImageGallery}>
+            <p>{`Something went wrong! ${error}`}</p>
+          </div>
+        )}
+
+        {showModal && (
+          <Modal onClose={this.toggleModal}>
+            <img src={this.handleData().imageURL} alt={this.handleData().alt} />
+          </Modal>
+        )}
+      </AppBox>
       );
     }
   }
